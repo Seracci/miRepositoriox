159a2e813efe45aa28fb152ead1809304445f130
Fer <Fer@DESKTOP-TM2PUHB>
2023-04-16 15:26:09 +0200

    Commit 1

---
+++ prueba/src/prueba/Prueba.java
@@ -0,0 +1,22 @@
+package prueba;
+
+public class Prueba {
+	private String nombre;
+	
+	public Prueba() {
+		this.nombre = "Prueba";
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "Prueba [nombre=" + nombre + "]";
+	}
+}
